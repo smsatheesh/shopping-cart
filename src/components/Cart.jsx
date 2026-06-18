d87fb96da1030fade7b431492b1eff1b53ab42f2
smsatheesh <smsatheesh2000@gmail.com>
2026-06-18 21:34:04 +0530

    feat: Project setup

---
+++ src/components/Cart.jsx
@@ -0,0 +1,60 @@
+import { useContext } from "react";
+import { CartContext } from "../store/shopping-card-context";
+
+export default function Cart() {
+  // const { items } = useContext(CartContext);
+
+  return (
+    <CartContext.Consumer>
+      {(cartContext) => {
+        const totalPrice = cartContext.items.reduce(
+          (acc, item) => acc + item.price * item.quantity,
+          0,
+        );
+        const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+
+        return (
+          <div id="cart">
+            {cartContext.items.length === 0 && <p>No items in cart!</p>}
+            {cartContext.items.length > 0 && (
+              <ul id="cart-items">
+                {cartContext.items.map((item) => {
+                  const formattedPrice = `$${item.price.toFixed(2)}`;
+
+                  return (
+                    <li key={item.id}>
+                      <div>
+                        <span>{item.name}</span>
+                        <span> ({formattedPrice})</span>
+                      </div>
+                      <div className="cart-item-actions">
+                        <button
+                          onClick={() =>
+                            cartContext.updateItemQuantity(item.id, -1)
+                          }
+                        >
+                          -
+                        </button>
+                        <span>{item.quantity}</span>
+                        <button
+                          onClick={() =>
+                            cartContext.updateItemQuantity(item.id, 1)
+                          }
+                        >
+                          +
+                        </button>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+            <p id="cart-total-price">
+              Cart Total: <strong>{formattedTotalPrice}</strong>
+            </p>
+          </div>
+        );
+      }}
+    </CartContext.Consumer>
+  );
+}
---
+++ src/store/shopping-card-context.jsx
@@ -0,0 +1,108 @@
+import { createContext, useState, useReducer } from "react";
+
+import { DUMMY_PRODUCTS } from "../dummy-products";
+
+export const CartContext = createContext({
+  items: [],
+  addItemToCart: () => {},
+  updateItemQuantity: () => {},
+});
+
+function shoppingCartReducer(state, action) {
+  if (action.identifier === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload,
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      console.log(action.payload);
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload,
+      );
+      updatedItems.push({
+        id: action.payload,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      items: updatedItems,
+    };
+  } else if (action.identifier === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.id,
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+    };
+  }
+
+  return state;
+}
+
+export default function CartContextProvider({ children }) {
+  //   const [shoppingCart, setShoppingCart] = useState({
+  //     items: [],
+  //   });
+
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(
+    shoppingCartReducer,
+    {
+      items: [],
+    },
+  );
+
+  function handleAddItemToCart(id) {
+    shoppingCartDispatch({
+      identifier: "ADD_ITEM",
+      payload: id,
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    shoppingCartDispatch({
+      identifier: "UPDATE_ITEM",
+      payload: {
+        id: productId,
+        amount,
+      },
+    });
+  }
+
+  const cartContextValue = {
+    items: shoppingCartState.items,
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={cartContextValue}>
+      {children}
+    </CartContext.Provider>
+  );
+}
